106f0f19495b29407c47d0e20da4709ceb2cbb36
Hasarel <hasarelrosenberg@gmail.com>
2020-01-20 16:20:08 +0530

    2020.01.20 Create driver map list new

--- app/src/main/java/com/nibm/rwp/gms/interfaces/EndPoints.java
+++ app/src/main/java/com/nibm/rwp/gms/interfaces/EndPoints.java
@@ -1,5 +1,7 @@
 package com.nibm.rwp.gms.interfaces;
 
+import com.google.gson.JsonElement;
+import com.nibm.rwp.gms.dto.CustomerRequest;
 import com.nibm.rwp.gms.dto.GarbageCategoryList;
 import com.nibm.rwp.gms.dto.GarbageRequest;
 import com.nibm.rwp.gms.dto.UcArea;
@@ -8,6 +10,7 @@ import com.nibm.rwp.gms.dto.UcVehicleList;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
@@ -22,21 +25,13 @@ public interface EndPoints {
     @GET("/category/all")
     Call<List<GarbageCategoryList>> getGarbageList();
 
+    @GET("/customer/all/request")
+    Call<List<CustomerRequest>> getDriverMap();
+
     @GET("/vehicle/types")
     Call<List<UcVehicleList>> getUcVehicle();
 
     @POST("/customer/request")
-    @FormUrlEncoded
-    Call<GarbageRequest> setCustomerRequest(
-            @Field("customer_name") String customer_name, @Field("email") String email,
-            @Field("area_id") String area_id, @Field("longitude") String longitude,
-            @Field("latitude") String latitude, @Field("address_1") String address_1,
-            @Field("address_2") String address_2, @Field("address_3") String address_3,
-            @Field("tele_no") String tele_no, @Field("description") String description,
-            @Field("category_id") String category_id, @Field("vehicle_type_id") String vehicle_type_id);
-
-    @POST("/customer/request")
-    Call<GarbageRequest> setCustomerRequest(GarbageRequest garbageRequestModel);
-
+    Call<JsonElement> setCustomerRequest(@Body GarbageRequest req);
 
 }
